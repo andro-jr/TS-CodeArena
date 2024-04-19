0d672a60ab9b3773eb342719bebad40595d9f608
andro-jr <prabin20panta@gmail.com>
2024-04-19 20:28:11 +0545

    additional types

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,3 +1,18 @@
 let apples: number | string = 5;
 
 apples = "5";
+
+let color: (string | number)[] = [9, "7"];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: string } = {
+  x: 4,
+  y: "7",
+};
+
+const logNumber: (i: number) => void = (i) => {
+  console.log(i);
+};
