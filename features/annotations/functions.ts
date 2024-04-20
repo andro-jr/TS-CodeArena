33882391a35218a1f79b2fb8cc8bc465402b4117
andro-jr <prabin20panta@gmail.com>
2024-04-20 11:14:57 +0545

    void, never and destructuring

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,37 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const divide = function divide(a: number, b: number): number {
+  return a / b;
+};
+
+// console.log(divide(20, 2));
+
+const logger = (message: string): void => {
+  console.log(message);
+  //   return undefined
+  //   return null
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const forecast = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(forecast);
